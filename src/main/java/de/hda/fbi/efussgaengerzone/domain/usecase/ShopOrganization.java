40ee2f6e7dbf4065930532b6c9e4bf8d66f496ca
stquannnn <quoccuong.an@stud.h-da.de>
2023-06-05 11:19:52 +0200

    prak9

--- src/main/java/de/hda/fbi/efussgaengerzone/domain/usecase/ShopOrganization.java
+++ src/main/java/de/hda/fbi/efussgaengerzone/domain/usecase/ShopOrganization.java
@@ -2,6 +2,7 @@ package de.hda.fbi.efussgaengerzone.domain.usecase;
 
 import de.hda.fbi.efussgaengerzone.domain.model.shop.Shop;
 import de.hda.fbi.efussgaengerzone.domain.model.shop.ShopRepository;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -9,21 +10,35 @@ import java.util.UUID;
 @Service
 public class ShopOrganization {
 
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(ShopOrganization.class);
     final ShopRepository shopRepository;
 
     public ShopOrganization(ShopRepository shopRepository) {
         this.shopRepository = shopRepository;
     }
 
-    /*
-    der Termin wurde im Repository-Mock gespeichert
-     */
-    public void createShop(Shop shop) {
 
+    public void createShop(Shop shop) {
+        log.debug("Create shop {}", shop);
+        shopRepository.save(shop);
     }
 
-    public void updateShopEmail(UUID shopId, String newMail) {
-
+    public void updateShopEmail(UUID shopId, String newMail) throws ShopNotFoundException {
+        log.debug("changing shop with id {} to render {}", shopId, newMail);
+        var shop = shopRepository.findById(shopId)
+                .orElseThrow(() -> new ShopNotFoundException(shopId));
+        var shopWithNewEmail = new Shop(
+                shop.id(),
+                shop.name(),
+                shop.description(),
+                shop.supportedVideoMessengers(),
+                shop.weeklyOpeningHours(),
+                shop.tags(),
+                shop.active(),
+                shop.minsPerCustomer(),
+                newMail
+        );
+        changeShop(shopWithNewEmail);
     }
 
     @SuppressWarnings("squid:S2201")
@@ -31,6 +46,7 @@ public class ShopOrganization {
         shopRepository.findById(shop.id())
                 .orElseThrow(() -> new ShopNotFoundException(shop.id()));
         shopRepository.save(shop);
+
     }
 
 }
